7561b20ca7a816dbe3818b544bc359f287c3a1c2
Radames Vaz <rgvtovar@hotmail.com>
2020-08-12 11:09:55 -0400

    ordenando fechas

--- controllers/BuscarPosts.js
+++ controllers/BuscarPosts.js
@@ -1,6 +1,11 @@
 const handleHomeBlog = (req, res, db) => {
     db.select().table('blog')
     .then(response => {
+            response.sort(function(a,b){
+            // Turn your strings into dates, and then subtract them
+            // to get a value that is either negative, positive, or zero.
+            return new Date(a.fecha) - new Date(b.fecha);
+          });
         res.json(response);
     })
     .catch(err => res.status(400).json(err));
